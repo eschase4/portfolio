02be96f3c2ca403c1da141323a44edccd493745e
Eli Chase <eschase4@gmail.com>
2023-07-29 18:09:45 -0400

    added musician match to projects, reordered them

--- src/data.js
+++ src/data.js
@@ -2,11 +2,25 @@ const movieFinderPreview = process.env.PUBLIC_URL + '/assets/Alabaster-Alligator
 const playlistPreview = process.env.PUBLIC_URL + '/assets/myPlaylist.png';
 const techBlogPreview = process.env.PUBLIC_URL + '/assets/techblog.png';
 const forecastPreview = process.env.PUBLIC_URL + '/assets/Forecast.png';
-const portfolioPreview = process.env.PUBLIC_URL + '/assets/firstportfolio.png';
 const readmePreview = process.env.PUBLIC_URL + '/assets/readme.png';
 const creepyPreview = process.env.PUBLIC_URL + '/assets/creepyPost.png';
+const musicPreview = process.env.PUBLIC_URL + '/assets/MusicMatch.png';
 
 const projects = [
+    {
+        name: "Musician Match",
+        link: "https://enigmatic-woodland-26963.herokuapp.com/",
+        preview: musicPreview,
+        description: "Powered by React, users can make an account, and match other musicians in their area. They can then message them, and set up a jam session. Essentially a dating app for musicians. Uses React, Node.js, Apollo Server, and GraphQL.",
+        repoLink: "https://github.com/eschase4/Musician-Match"
+    },
+    {
+        name: "CreepyPostBot",
+        link: "https://twitter.com/CreepyPostBot",
+        preview: creepyPreview,
+        description: "My first expirement with OpenAI's GPT-3 API. Using a cron job to set an interval, the bot will post a random AI generated creepy story to a twitter account every 4 hours. Written in Node.js, and hosted by Heroku.",
+        repoLink: "https://github.com/eschase4/CreepyPostsBot"
+    },
     {
         name: "Movie Finder",
         link: "https://eschase4.github.io/Alabaster-Alligators/",
@@ -34,20 +48,6 @@ const projects = [
         preview: forecastPreview,
         description: "My first use of third-party API's; users can enter a location, and recieve a five day forecast for the requested area.",
         repoLink: "https://github.com/eschase4/Weather-App"
-    },
-    {
-        name: "ReadMe Maker",
-        link: "https://github.com/eschase4/README-Generator",
-        preview: readmePreview,
-        description: "One of my most useful projects; users can clone the app from github, and use their CLI to answer a questionaire and have a professional ReadMe Generated for them.",
-        repoLink: "https://github.com/eschase4/README-Generator"
-    },
-    {
-        name: "CreepyPostBot",
-        link: "https://twitter.com/CreepyPostBot",
-        preview: creepyPreview,
-        description: "My first expirement with OpenAI's GPT-3 API. Using a cron job to set an interval, the bot will post a random AI generated creepy story to a twitter account every 4 hours. Written in Node.js, and hosted by Heroku.",
-        repoLink: "https://github.com/eschase4/CreepyPostsBot"
     },
 
 ]
