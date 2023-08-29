182800795ea4911287a9713eead97865b37b9774
Eli Chase <eschase4@gmail.com>
2023-08-28 22:29:04 -0400

    added book search to projects

--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
 const movieFinderPreview = process.env.PUBLIC_URL + '/assets/Alabaster-Alligator_sc.png'; //624x506
-const playlistPreview = process.env.PUBLIC_URL + '/assets/myPlaylist.png';
-const techBlogPreview = process.env.PUBLIC_URL + '/assets/techblog.png';
-const forecastPreview = process.env.PUBLIC_URL + '/assets/Forecast.png';
 const creepyPreview = process.env.PUBLIC_URL + '/assets/CreepyPost.png';
 const musicPreview = process.env.PUBLIC_URL + '/assets/MusicMatch.png';
+const bookSearchPreview = process.env.PUBLIC_URL + '/assets/bookSearch.png';
+// const playlistPreview = process.env.PUBLIC_URL + '/assets/myPlaylist.png';
+// const techBlogPreview = process.env.PUBLIC_URL + '/assets/techblog.png';
+// const forecastPreview = process.env.PUBLIC_URL + '/assets/Forecast.png';
 
 const projects = [
     {
@@ -21,34 +22,33 @@ const projects = [
         repoLink: "https://github.com/eschase4/CreepyPostsBot"
     },
     {
-        name: "Movie Finder",
-        link: "https://eschase4.github.io/Alabaster-Alligators/",
-        preview: movieFinderPreview,
-        description: "An online Quiz that suggests a movie to users, and tells them where it is streaming. Made with JavaScript and jQuery.",
-        repoLink: "https://github.com/eschase4/Alabaster-Alligators"
-    },
-    {
-        name: "Playlist Maker",
-        link: "https://enigmatic-island-78855.herokuapp.com/",
-        preview: playlistPreview,
-        description: "Users can login and manage a personalized playlist. Uses Express.js, Node.js, Sequelize, and Handlebars.",
-        repoLink: "https://github.com/eschase4/Green-Parade"
-    },
-    {
-        name: "Tech Blog",
-        link: "https://fathomless-thicket-47575.herokuapp.com/home",
-        preview: techBlogPreview,
-        description: "Users can create an account, and post to a blog. Uses Express.js, Node.js, Sequelize, and Handlebars.",
-        repoLink: "https://github.com/eschase4/Tech-Blog"
-    },
-    {
-        name: "Weather Tracker",
-        link: "https://eschase4.github.io/Weather-App/",
-        preview: forecastPreview,
-        description: "Users can search for a city and recieve it's forecast for the next five days. Uses JavaScript, and the OpenWeather API",
-        repoLink: "https://github.com/eschase4/Weather-App"
-    },
-
+        name: "Book Search",
+        link: "https://github.com/eschase4/Book-Search-Engine",
+        preview: bookSearchPreview,
+        description: "Users can search for books and save them to their profile. Uses React.js, Node.js, Apollo Server, GraphQL, and MongoDB.",
+        repoLink: "https://evening-scrubland-55937-9cdd0feb7b88.herokuapp.com/"
+    }//,
+//     {
+//         name: "Playlist Maker",
+//         link: "https://enigmatic-island-78855.herokuapp.com/",
+//         preview: playlistPreview,
+//         description: "Users can login and manage a personalized playlist. Uses Express.js, Node.js, Sequelize, and Handlebars.",
+//         repoLink: "https://github.com/eschase4/Green-Parade"
+//     },
+//     {
+//         name: "Tech Blog",
+//         link: "https://fathomless-thicket-47575.herokuapp.com/home",
+//         preview: techBlogPreview,
+//         description: "Users can create an account, and post to a blog. Uses Express.js, Node.js, Sequelize, and Handlebars.",
+//         repoLink: "https://github.com/eschase4/Tech-Blog"
+//     },
+//     {
+//         name: "Weather Tracker",
+//         link: "https://eschase4.github.io/Weather-App/",
+//         preview: forecastPreview,
+//         description: "Users can search for a city and recieve it's forecast for the next five days. Uses JavaScript, and the OpenWeather API",
+//         repoLink: "https://github.com/eschase4/Weather-App"
+//     },
 ]
 
 export default projects ;
